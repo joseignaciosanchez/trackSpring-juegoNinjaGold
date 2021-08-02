347e65d8f22f317b6c01485d136299058ef3765d
Your Name <you@example.com>
2021-08-02 01:53:02 -0400

    ...

---
+++ src/test/java/com/codingdojo/JuegoNinjaGold/JuegoNinjaGoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.JuegoNinjaGold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegoNinjaGoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
